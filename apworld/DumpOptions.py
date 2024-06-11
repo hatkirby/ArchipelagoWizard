a8b67caaf98dccba9e32d8671c5bfbf147e883cb
Star Rauchenberger <fefferburbia@gmail.com>
2024-06-11 11:42:20 -0400

    Create apworld

---
+++ apworld/DumpOptions.py
@@ -0,0 +1,161 @@
+import argparse
+import json
+import logging
+import typing
+
+import Options
+
+common_option_names = ["start_inventory", "local_items", "non_local_items", "start_hints", "start_location_hints",
+                       "exclude_locations", "priority_locations", "start_inventory_from_pool", "item_links"]
+
+
+def get_html_doc(option_type: type(Options.Option)) -> str:
+    if not option_type.__doc__:
+        return "Please document me!"
+    return "\n".join(line.strip() for line in option_type.__doc__.split("\n")).strip()
+
+
+def dump(output_path: str):
+    options_output = {}
+
+    from worlds.AutoWorld import AutoWorldRegister
+
+    for game_name, world in AutoWorldRegister.world_types.items():
+        if game_name == "Archipelago":
+            continue
+
+        all_options: typing.Dict[str, Options.AssembleOptions] = world.options_dataclass.type_hints
+
+        game_options = {}
+        common_options = []
+        for option_name, option in all_options.items():
+            if option_name in common_option_names:
+                common_options.append(option_name)
+
+            elif issubclass(option, Options.Choice) or issubclass(option, Options.Toggle):
+                game_options[option_name] = this_option = {
+                    "type": "text-select" if issubclass(option, Options.TextChoice) else "select",
+                    "displayName": option.display_name if hasattr(option, "display_name") else option_name,
+                    "description": get_html_doc(option),
+                    "defaultValue": None,
+                    "options": []
+                }
+
+                for sub_option_id, sub_option_name in option.name_lookup.items():
+                    if sub_option_name != "Random":
+                        this_option["options"].append({
+                            "id": sub_option_id,
+                            "name": option.get_option_name(sub_option_id),
+                            "value": sub_option_name,
+                        })
+                    if sub_option_id == option.default:
+                        this_option["defaultValue"] = sub_option_name
+
+                for alias_name, sub_option_id in option.aliases.items():
+                    this_option.setdefault("aliases", []).append({
+                        "name": alias_name,
+                        "value": option.name_lookup[sub_option_id]
+                    })
+
+                if not this_option["defaultValue"]:
+                    this_option["defaultValue"] = "random"
+
+            elif issubclass(option, Options.Range):
+                game_options[option_name] = {
+                    "type": "range",
+                    "displayName": option.display_name if hasattr(option, "display_name") else option_name,
+                    "description": get_html_doc(option),
+                    "defaultValue": option.default if hasattr(
+                        option, "default") and option.default != "random" else option.range_start,
+                    "min": option.range_start,
+                    "max": option.range_end,
+                }
+
+                if issubclass(option, Options.NamedRange):
+                    game_options[option_name]["type"] = 'named_range'
+                    game_options[option_name]["value_names"] = {}
+                    for key, val in option.special_range_names.items():
+                        game_options[option_name]["value_names"][key] = val
+
+            elif issubclass(option, Options.ItemSet):
+                game_options[option_name] = {
+                    "type": "items-set",
+                    "displayName": option.display_name if hasattr(option, "display_name") else option_name,
+                    "description": get_html_doc(option),
+                    "defaultValue": list(option.default)
+                }
+
+            elif issubclass(option, Options.ItemDict):
+                game_options[option_name] = {
+                    "type": "items-dict",
+                    "displayName": option.display_name if hasattr(option, "display_name") else option_name,
+                    "description": get_html_doc(option),
+                    "defaultValue": list(option.default)
+                }
+
+            elif issubclass(option, Options.LocationSet):
+                game_options[option_name] = {
+                    "type": "locations-set",
+                    "displayName": option.display_name if hasattr(option, "display_name") else option_name,
+                    "description": get_html_doc(option),
+                    "defaultValue": list(option.default)
+                }
+
+            elif issubclass(option, Options.OptionDict):
+                game_options[option_name] = {
+                    "type": "options-dict",
+                    "displayName": option.display_name if hasattr(option, "display_name") else option_name,
+                    "description": get_html_doc(option),
+                    "options": list(option.valid_keys),
+                    "defaultValue": list(option.default) if hasattr(option, "default") else []
+                }
+
+            elif issubclass(option, Options.OptionList) or issubclass(option, Options.OptionSet):
+                if option.valid_keys:
+                    game_options[option_name] = {
+                        "type": "options-set",
+                        "displayName": option.display_name if hasattr(option, "display_name") else option_name,
+                        "description": get_html_doc(option),
+                        "options": list(option.valid_keys),
+                        "defaultValue": list(option.default) if hasattr(option, "default") else []
+                    }
+                else:
+                    game_options[option_name] = {
+                        "type": "free-set",
+                        "displayName": option.display_name if hasattr(option, "display_name") else option_name,
+                        "description": get_html_doc(option),
+                        "defaultValue": list(option.default) if hasattr(option, "default") else []
+                    }
+
+            else:
+                logging.debug(f"{option} not exported.")
+
+        options_output[game_name] = {
+            "options": game_options,
+            "commonOptions": common_options,
+            "items": tuple(world.item_names),
+            "itemGroups": [
+                group for group in world.item_name_groups.keys() if group != "Everything"
+            ],
+            "itemDescriptions": world.item_descriptions if hasattr(world, "item_descriptions") else {},
+            "locations": tuple(world.location_names),
+            "locationGroups": [
+                group for group in world.location_name_groups.keys() if group != "Everywhere"
+            ],
+            "locationDescriptions": world.location_descriptions if hasattr(world, "location_descriptions") else {},
+            "presets": world.web.options_presets,
+        }
+
+    with open(output_path, "w") as f:
+        json.dump(options_output, f, indent=2, separators=(',', ': '))
+
+    logging.info(f"Done! Output written to {output_path}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="World options dumper")
+    parser.add_argument('--output_path', default="dumped-options.json",
+                        help='Path to the file where the dumped options should be written')
+    args = parser.parse_args()
+
+    dump(args.output_path)
---
+++ apworld/__init__.py
@@ -0,0 +1,77 @@
+import asyncio
+import logging
+import os
+import threading
+
+import Utils
+
+if Utils.is_frozen():
+    os.environ["KIVY_DATA_DIR"] = Utils.local_path("data")
+
+from kivy.app import App
+from kivy.uix.layout import Layout
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+
+from kvui import ContainerLayout, UILog, MainLayout, TooltipLabel
+from worlds.AutoWorld import World
+from worlds.LauncherComponents import Component, Type, components, launch_subprocess
+from . import DumpOptions
+
+
+class SingleLabel(RecycleDataViewBehavior, TooltipLabel):
+    pass
+
+
+class WizardConsoleGui(App):
+    container: Layout
+    exit_event: asyncio.locks.Event
+    t1: threading.Thread
+
+    def __init__(self):
+        self.title = "Dump Options"
+        self.exit_event = asyncio.Event()
+
+        super().__init__()
+
+    def build(self) -> Layout:
+        self.container = ContainerLayout()
+
+        ui_log = UILog(logging.getLogger())
+        ui_log.viewclass = SingleLabel
+
+        grid = MainLayout()
+        grid.cols = 1
+        grid.add_widget(ui_log)
+        self.container.add_widget(grid)
+
+        return self.container
+
+    def on_start(self):
+        t1 = threading.Thread(target=DumpOptions.dump, args=["dumped-options.json"])
+        t1.start()
+
+    def on_stop(self):
+        self.exit_event.set()
+
+
+def launch_client_helper():
+    async def main():
+        gui = WizardConsoleGui()
+        gui_task = asyncio.create_task(gui.async_run(), name="DumpOptionsGui")
+
+        await gui.exit_event.wait()
+        await gui_task
+
+    asyncio.run(main())
+
+
+def launch_client():
+    launch_subprocess(launch_client_helper, "DumpOptions")
+
+
+components.append(Component("Dump Options for Wizard", "DumpOptions", func=launch_client, component_type=Type.TOOL))
+
+
+class ApWizardWorld(World):
+    item_name_to_id = {}
+    location_name_to_id = {}
